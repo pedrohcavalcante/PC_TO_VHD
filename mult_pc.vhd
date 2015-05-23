e979f27e572b521f949b90920022076bb9c6869f
Pedro Henrique Bezerra Cavalcante <pedrohbcavalcante@outlook.com>
2015-05-23 18:44:27 -0300

    Final das linhas
    S7
    Falta terminar o comparador e as saídas

--- mult_pc.vhd
+++ mult_pc.vhd
@@ -26,4 +26,12 @@ with reset select
 	S5 <= S4 when '0',
 		  '0' when others;
 with Q select
-		  
+	S7 <= '0' & start & start & '0' & '0' & '0' when '00',
+		  '00' when '000', 
+		  '01' when '001', 
+		  '22' when '010', 
+		  '44' when '011', 
+		  '09' when '100', 
+		  '84' when '101', 
+		  '11' when '110';
+-- Falta terminar o comparador e fazer as saídas
